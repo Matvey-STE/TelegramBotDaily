dfba7699406ef18fc3bf9cc13aedcd8e62f07a3d
Matvey Stetcenko <matveyvs1987@gmail.com>
2023-05-01 16:05:21 +0200

    Tried to send message to telegram bot. Change version of telegram bot from 5.0.1 to 6.3.0 because of error appearing.

--- dispatcher/src/main/java/com/matveyvs1987/controller/TelegramBot.java
+++ dispatcher/src/main/java/com/matveyvs1987/controller/TelegramBot.java
@@ -1,11 +1,13 @@
 package com.matveyvs1987.controller;
 
 import lombok.extern.log4j.Log4j;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 @Log4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
@@ -27,5 +29,19 @@ public class TelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         var testMessage = update.getMessage();
         log.debug(testMessage.getText());
+
+        var response = new SendMessage();
+        response.setChatId(testMessage.getChatId().toString());
+        response.setText("Hello my dear friend " + testMessage.getChat().getFirstName());
+        sendAnswerMessage(response);
+    }
+    public void sendAnswerMessage(SendMessage message){
+        if (message != null){
+            try {
+                execute(message);
+            } catch (TelegramApiException e){
+                log.error("Send answer message error " + e);
+            }
+        }
     }
 }
