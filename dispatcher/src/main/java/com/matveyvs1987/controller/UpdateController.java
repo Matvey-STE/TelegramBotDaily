fa4a1cb4c5bc3f3c691c4952c5a1322e99328dc8
Matvey Stetcenko <matveyvs1987@gmail.com>
2023-05-01 23:27:37 +0200

    Add update and message validation

---
+++ dispatcher/src/main/java/com/matveyvs1987/controller/UpdateController.java
@@ -0,0 +1,65 @@
+package com.matveyvs1987.controller;
+
+import com.matveyvs1987.utils.MessageUtils;
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+@Log4j
+public class UpdateController {
+    private TelegramBot telegramBot;
+    private MessageUtils messageUtils;
+
+    public UpdateController(MessageUtils messageUtils) {
+        this.messageUtils = messageUtils;
+    }
+
+    public void registerBot(TelegramBot telegramBot){
+        this.telegramBot = telegramBot;
+    }
+    public void processUpdate(Update update){
+        if (update == null){
+            log.error("Received update is null");
+        }
+        if (update.getMessage() != null){
+            distributeMessageByType(update);
+        } else {
+            log.error("Received unsupported message type " + update);
+        }
+    }
+
+    private void distributeMessageByType(Update update) {
+        var message = update.getMessage();
+        if (message.getText() != null){
+            processTextMessage(update);
+        } else if (message.getDocument() != null){
+            processDocMessage(update);
+        } else if (message.getPhoto() != null){
+            processPhotoMessage(update);
+        } else {
+            setUnsupportedMessageTypeView(update);
+        }
+    }
+
+    private void setUnsupportedMessageTypeView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update,
+                "Unsupported type of message!");
+        setView(sendMessage);
+    }
+    private void setView(SendMessage sendMessage) {
+        telegramBot.sendAnswerMessage(sendMessage);
+    }
+    private void processPhotoMessage(Update update) {
+        
+    }
+
+    private void processDocMessage(Update update) {
+        
+    }
+
+    private void processTextMessage(Update update) {
+        
+    }
+}
